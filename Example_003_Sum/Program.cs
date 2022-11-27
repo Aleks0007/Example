323a1a359d1e068247935ce836e8caabd3a02646
AlekseiShvaiko <aarkaklim@gmail.com>
2022-11-27 23:22:39 +0400

    Added random

--- Example_003_Sum/Program.cs
+++ Example_003_Sum/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите два натуральных числа");
-int firstA = 5;
-int secondB = 8;
+﻿int firstA = new Random().Next(1, 11);
+Console.WriteLine(firstA);
+int secondB = new Random().Next(1, 18);
+Console.WriteLine(secondB);
 Console.WriteLine(firstA + secondB);
