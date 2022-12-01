0afc65b80f10ed052a87e543d41c7b2251e0f97b
AlekseiShvaiko <aarkaklim@gmail.com>
2022-12-01 16:52:17 +0400

    Added Seminar_001

---
+++ Seminar_001/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if ((99 < a < 1000) || (-99 > a > -1000))
+{
+Console.Write(a % 10);
+}
+else
+{
+Console.Write("Число не является трёхзначным");
+}
