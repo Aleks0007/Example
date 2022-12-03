d727b2e893e3e378b1ee5c9809758d0136e57879
AlekseiShvaiko <aarkaklim@gmail.com>
2022-12-03 18:09:32 +0400

    Added Seminar_007

---
+++ Seminar_007/Program.cs
@@ -0,0 +1,21 @@
+﻿int num = 1;
+while (num <= 100)
+{
+    if (num % 15 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (num % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else if (num % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else
+    {
+         Console.WriteLine(num);
+    }
+    num++;
+}
