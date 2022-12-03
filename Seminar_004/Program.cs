2f3ba19ce30290ecd9ecb4bbc9aed980d516e543
AlekseiShvaiko <aarkaklim@gmail.com>
2022-12-03 16:39:47 +0400

    Added Seminar_004

---
+++ Seminar_004/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+int num3 = num2 % num1;
+
+if (num2 % num1 == 0)
+{
+    Console.WriteLine("Второе число кратно первому");
+}
+else
+{
+    Console.WriteLine($"Второе число не кратно первому, остаток --> {num3}");
+}
