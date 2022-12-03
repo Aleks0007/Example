2f21e37400056b491fd0f28386af9fd1b347f0f7
AlekseiShvaiko <aarkaklim@gmail.com>
2022-12-03 15:19:12 +0400

    Added Seminar_002

---
+++ Seminar_002/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = new Random().Next(10, 100); 
+int number1 = number / 10;
+int number2 = number % 10;
+int max = number1;
+
+if (number2 > max)
+{
+    max = number2;
+}
+Console.WriteLine($"Максимальная цифра в числе {number} --> {max}");
