bb13fc6a8de11ef40d9008b792ab0afc42fd0186
AlekseiShvaiko <aarkaklim@gmail.com>
2022-12-07 17:30:06 +0400

    Added Seminar_011

---
+++ Seminar_011/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+от 1 до N. */
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int i = 1;
+
+while (i <= Math.Abs(N))
+{
+    Console.Write(Math.Pow(i, 2));
+    Console.Write(" ");
+    i++;
+}
