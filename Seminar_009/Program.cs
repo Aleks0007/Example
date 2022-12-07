e23d0db2da9b03dd028c31ee8825c2056314a66d
AlekseiShvaiko <aarkaklim@gmail.com>
2022-12-07 16:21:30 +0400

    Added Seminar_009

---
+++ Seminar_009/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите номер четверти: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num == 1) Console.WriteLine("х > 0, y > 0");
+else if (num == 2) Console.WriteLine("х < 0, y > 0");
+else if (num == 3) Console.WriteLine("х < 0, y < 0");
+else if (num == 4) Console.WriteLine("х > 0, y < 0");
+else Console.WriteLine("Заданного номера четверти не существует");
