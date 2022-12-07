ed5af40e9775da51bcc58c7960a1c6eb5446293a
AlekseiShvaiko <aarkaklim@gmail.com>
2022-12-07 16:02:35 +0400

    Added Seminar_008

---
+++ Seminar_008/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите х: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Write("Введите у: ");
+int y = int.Parse(Console.ReadLine()!);
+
+if (x > 0 && y > 0) Console.WriteLine("Первая четверть");
+
+if (x < 0 && y > 0) Console.WriteLine("Вторая четверть");
+
+if (x < 0 && y < 0) Console.WriteLine("Третья четверть");
+
+else Console.WriteLine("Четвёртая четверть");
