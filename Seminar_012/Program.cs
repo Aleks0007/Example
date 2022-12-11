4ad5844f8ee8aa614bfa363e7e3d401a43a0acbc
AlekseiShvaiko <aarkaklim@gmail.com>
2022-12-11 14:21:00 +0400

    Added Seminar_012

---
+++ Seminar_012/Program.cs
@@ -0,0 +1,16 @@
+﻿ // Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+ Console.Write("Введите число: ");
+ int N = int.Parse(Console.ReadLine()!);
+ Console.WriteLine($"Сумма чисел равна {GetSumNums(N)}");
+
+ int GetSumNums(int number)
+ {
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number;
+        number--;
+    }
+    return sum;
+ }
