a46c6419f34b3638219e8c2424b8734a346cd6c9
AlekseiShvaiko <aarkaklim@gmail.com>
2022-12-16 19:39:48 +0400

    Added Example_014

---
+++ Example_014/Program.cs
@@ -0,0 +1,19 @@
+﻿/* int Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+Console.WriteLine(Factorial(3));
+
+// f(1) = 1, f(2) = 1, f(n) = f(n - 1) + f(n - 2) */
+
+int factorial(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return factorial(n - 1) + factorial(n - 2);
+}
+
+for (int i = 1; i < 15; i++)
+{
+    Console.WriteLine(factorial(i));
+}
