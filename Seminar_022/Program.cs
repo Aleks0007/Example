df90729a22b92f6473d0e59c353ea2eca8249715
AlekseiShvaiko <aarkaklim@gmail.com>
2022-12-18 16:12:11 +0400

    Added Seminar_022

---
+++ Seminar_022/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int res = 0;
+string answer = "";
+Console.WriteLine(Get2Num(num));
+
+string Get2Num(int num1)
+{
+    while (num1 > 0)
+    {
+        res = num1 % 2;
+        num1 /= 2;
+        answer = res + answer;
+    }
+    return answer;
+}
